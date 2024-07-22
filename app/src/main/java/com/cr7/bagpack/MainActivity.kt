dedc0b0dfeb8b7547d6dd8fe3b05650dfa9bc7ba
Chenna-CR <chennacr777@gmail.com>
2024-07-22 13:10:06 +0530

    bag pack

--- app/src/main/java/com/cr7/bagpack/MainActivity.kt
+++ app/src/main/java/com/cr7/bagpack/MainActivity.kt
@@ -6,30 +6,38 @@ import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.cr7.bagpack.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
     private lateinit var binding: ActivityMainBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        setSupportActionBar(binding.toolbar)
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.host) as NavHostFragment
         navController = navHostFragment.navController
 
-        val appBarConfiguration = AppBarConfiguration.Builder(
-            R.id.homeFragment,
-            R.id.tripPlanningFragment,
-            R.id.packingListFragment,
-            R.id.expenseTrackerFragment,
-            R.id.calendarFragment
-        ).build()
-
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+navController.addOnDestinationChangedListener{_, destination, _ ->
+    when(destination.id){
+        R.id.homeFragment-> binding.tittleToolbar.setText("Bagpack")
+        R.id.packingListFragment -> binding.tittleToolbar.setText("Packing List")
+        R.id.tripPlanningFragment -> binding.tittleToolbar.setText("Trip Planning")
+        R.id.calendarFragment-> binding.tittleToolbar.setText("Calendar")
+        R.id.expenseTrackerFragment-> binding.tittleToolbar.setText("Expense Tracker")
+    }
+}
 
 }
+    override fun onSupportNavigateUp(): Boolean {
+        return super.onSupportNavigateUp() || navController.popBackStack()
+    }
 }
